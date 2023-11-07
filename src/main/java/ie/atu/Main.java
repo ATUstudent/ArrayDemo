08ea55195065c6298edffe826336c688248b2f52
G00422935@atu.ie <G00422935@atu.ie>
2023-11-07 13:57:49 +0000

    Starting Array

---
+++ src/main/java/ie/atu/Main.java
@@ -0,0 +1,34 @@
+package ie.atu;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        int[] Array;
+
+        Array = new int [10];
+
+        Array[0] = 100;
+        Array[1] = 200;
+        Array[2] = 300;
+        Array[3] = 400;
+        Array[4] = 500;
+        Array[5] = 600;
+        Array[6] = 700;
+        Array[7] = 800;
+        Array[8] = 900;
+        Array[9] = 1000;
+
+        System.out.println("Element at Index 0: " + Array[0]);
+        System.out.println("Element at Index 1: " + Array[1]);
+        System.out.println("Element at Index 2: " + Array[2]);
+        System.out.println("Element at Index 3: " + Array[3]);
+        System.out.println("Element at Index 4: " + Array[4]);
+        System.out.println("Element at Index 5: " + Array[5]);
+        System.out.println("Element at Index 6: " + Array[6]);
+        System.out.println("Element at Index 7: " + Array[7]);
+        System.out.println("Element at Index 8: " + Array[8]);
+        System.out.println("Element at Index 9: " + Array[9]);
+    }
+}
